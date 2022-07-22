b99fd504ba95791c51a69d4b8212a57f3bd884d6
da-head0 <andyourturntome@gmail.com>
2022-07-23 00:21:37 +0530

    Updated User model

--- Routes/auth.js
+++ Routes/auth.js
@@ -6,8 +6,10 @@ const jwt = require("jsonwebtoken");
 //Register
 router.post("/register", async (req, res) => {
   const newUser = new User({
+    name: req.body.name,
     username: req.body.username,
     email: req.body.email,
+    phonenumber: req.body.phonenumber,
     password: CryptoJS.AES.encrypt(
       req.body.password,
       process.env.PASS_KEY
--- models/User.js
+++ models/User.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
+    phonenumber: { type: Number, unique: true, required: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
   },
